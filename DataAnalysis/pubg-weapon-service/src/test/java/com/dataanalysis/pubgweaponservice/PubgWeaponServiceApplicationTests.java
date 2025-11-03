a7d9697d5991183166d49f837bba3cbb49f09eb5
yuanhe0609 <15853976147@qq.com>
2025-11-03 13:33:57 +0800

    feat: init

---
+++ DataAnalysis/pubg-weapon-service/src/test/java/com/dataanalysis/pubgweaponservice/PubgWeaponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataanalysis.pubgweaponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubgWeaponServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
