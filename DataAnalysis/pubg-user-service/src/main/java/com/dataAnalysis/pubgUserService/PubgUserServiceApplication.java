238325d5d5d76c5c25fe6213def9fcd7d8a6997b
yuanhe0609 <15853976147@qq.com>
2025-11-03 13:45:21 +0800

    feat: init

---
+++ DataAnalysis/pubg-user-service/src/main/java/com/dataAnalysis/pubgUserService/PubgUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dataAnalysis.pubgUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubgUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubgUserServiceApplication.class, args);
+    }
+
+}
---
+++ DataAnalysis/pubg-user-service/src/test/java/com/dataAnalysis/pubgUserService/PubgUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataAnalysis.pubgUserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubgUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
