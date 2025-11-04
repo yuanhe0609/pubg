8eb249158852b70948087f94f6489f16a6c69725
yuanhe0609 <15853976147@qq.com>
2025-11-04 17:52:18 +0800

    feat: init

---
+++ DataAnalysis/pubg-common-service/src/main/java/com/dataAnalysis/pubgEntityRegister/PubgEntityRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.dataAnalysis.pubgEntityRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubgEntityRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubgEntityRegisterApplication.class, args);
+    }
+
+}
