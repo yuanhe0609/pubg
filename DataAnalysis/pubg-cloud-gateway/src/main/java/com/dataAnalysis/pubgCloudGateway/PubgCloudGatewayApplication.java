58131ee37c62737cf7507b9d883776664e5b7e23
yuanhe0609 <15853976147@qq.com>
2025-11-07 14:15:47 +0800

    feat: init

---
+++ DataAnalysis/pubg-cloud-gateway/src/main/java/com/dataAnalysis/pubgCloudGateway/PubgCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dataAnalysis.pubgCloudGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubgCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubgCloudGatewayApplication.class, args);
+    }
+
+}
---
+++ DataAnalysis/pubg-cloud-gateway/src/test/java/com/dataAnalysis/pubgCloudGateway/PubgCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataAnalysis.pubgCloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubgCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
